b7fd10fa5689e7a274853043e1bcd0945cfd6992
Rik Schennink <rikschennink@gmail.com>
2017-12-29 15:48:38 +0100

    add CustomEvent and Array.some polyfills

--- dist/filepond-polyfill.js
+++ dist/filepond-polyfill.js
@@ -204,6 +204,34 @@
     });
   }
 
+  // Production steps of ECMA-262, Edition 5, 15.4.4.17
+  // Reference: http://es5.github.io/#x15.4.4.17
+  if (!Array.prototype.some) {
+    Array.prototype.some = function(fun /*, thisArg*/) {
+      'use strict';
+
+      if (this == null) {
+        throw new TypeError('Array.prototype.some called on null or undefined');
+      }
+
+      if (typeof fun !== 'function') {
+        throw new TypeError();
+      }
+
+      var t = Object(this);
+      var len = t.length >>> 0;
+
+      var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+      for (var i = 0; i < len; i++) {
+        if (i in t && fun.call(thisArg, t[i], i, t)) {
+          return true;
+        }
+      }
+
+      return false;
+    };
+  }
+
   if (typeof Object.assign != 'function') {
     // Must be writable: true, enumerable: false, configurable: true
     Object.defineProperty(Object, 'assign', {
@@ -237,4 +265,29 @@
       configurable: true
     });
   }
+
+  // https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent
+  (function() {
+    if (typeof window.CustomEvent === 'function') return false;
+
+    function CustomEvent(event, params) {
+      params = params || {
+        bubbles: false,
+        cancelable: false,
+        detail: undefined
+      };
+      var evt = document.createEvent('CustomEvent');
+      evt.initCustomEvent(
+        event,
+        params.bubbles,
+        params.cancelable,
+        params.detail
+      );
+      return evt;
+    }
+
+    CustomEvent.prototype = window.Event.prototype;
+
+    window.CustomEvent = CustomEvent;
+  })();
 });
